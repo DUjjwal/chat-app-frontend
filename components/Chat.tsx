a792548ccb8e66b7dc8bd1150b41e35b0e41df1a
DUjjwal <ujjwaldhiman2004@gmail.com>
2025-07-05 22:05:26 +0530

    ujjwaldhiman

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -139,7 +139,6 @@ function Chat() {
             </div>  
             <div className="h-[10%] outline outline-blue-900 rounded-xl shadow flex w-[100%]">
                 <input type="text" placeholder="Type a message" className='text-base md:text-lg xl:text-xl outline-gray-500 rounded-xl w-[100%] active:outline outline-white p-2' onChange={(e) => setText(e.target.value)} onKeyDown={(e) => {
-                    console.log(text)
                     if(e.key === "Enter" && text!=="") {
                         socketRef.current?.emit("room-message", {
                             userName: username,
@@ -150,7 +149,7 @@ function Chat() {
                     }
                 }} value={text}/>
                 <button className="flex justify-center items-center text-sm md:text-xl xl:text-xl w-[10%] bg-blue-500 rounded-r-xl text-white p-2 flex justify-center" onClick={() => {
-                    if(text!=="")return;
+                    if(text==="")return;
                     socketRef.current?.emit("room-message", {
                         userName: username,
                         roomID: room,
